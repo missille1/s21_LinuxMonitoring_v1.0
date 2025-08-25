b55a15f8425c30e66adf3efb75218b64a5467c76
Крохмалев Константин Олегович <konstantin.krohmalev@nami.ru>
2025-08-25 21:27:39 +0300

    04 finish

--- src/04/color.sh
+++ src/04/color.sh
@@ -28,3 +28,78 @@ print_text() {
         "$(paint "$color1" "$color2" "$label")" \
         "$(paint "$color3" "$color4" "$value")"
 }
+
+# получаем имена цветом из цифр .conf
+color_name() {
+    case "$1" in
+        1) echo "white" ;;
+        2) echo "red" ;;
+        3) echo "green" ;;
+        4) echo "blue" ;;
+        5) echo "purple" ;;
+        6) echo "black" ;;
+    esac
+}
+
+print_scheme_line() {
+    local title="$1" num="$2" is_def="$3"
+    local name; name=$(color_name "$num")
+    if [[ "$is_def" -eq 1 ]]; then
+        printf '%s = default (%s)\n' "$title" "$name"
+    else
+        printf '%s = %s (%s)\n' "$title" "$num" "$name"
+    fi
+}
+
+apply_color_defaults() {
+
+    # defaults colors
+    default_color1_cb=6 # black
+    default_color1_cl=1 # white
+    default_color2_cb=2 # red
+    default_color2_cl=4 # blue
+
+    # цвет фона первой колонки
+    if ! [[ -v column1_background ]]; then
+        column1_background="$default_color1_cb"
+        is_def_color1_cb=1
+    elif [ "$column1_background" = "" ]; then
+        column1_background="$default_color1_cb"
+        is_def_color1_cb=1
+    else
+        is_def_color1_cb=0
+    fi
+
+    # цвет шрифта первой колонки
+    if ! [[ -v column1_letter_color ]]; then
+        column1_letter_color="$default_color1_cl"
+        is_def_color1_cl=1
+    elif [ "$column1_letter_color" = "" ]; then
+        column1_letter_color="$default_color1_cl"
+        is_def_color1_cl=1
+    else
+        is_def_color1_cl=0
+    fi
+
+    # цвет фона второй колонки
+    if ! [[ -v column2_background ]]; then
+        column2_background="$default_color2_cb"
+        is_def_color2_cb=1
+    elif [ "$column2_background" = "" ]; then
+        column2_background="$default_color2_cb"
+        is_def_color2_cb=1
+    else
+        is_def_color2_cb=0
+    fi
+
+    # цвет фона второй колонки
+    if ! [[ -v column2_letter_color ]]; then
+        column2_letter_color="$default_color2_cl"
+        is_def_color2_cl=1
+    elif [ "$column2_letter_color" = "" ]; then
+        column2_letter_color="$default_color2_cl"
+        is_def_color2_cl=1
+    else
+        is_def_color2_cl=0
+    fi
+}
--- src/04/main.sh
+++ src/04/main.sh
@@ -4,7 +4,26 @@
 . ./color.sh
 . ./func.sh
 
-# $@ - "S1", "S2" ...
-param "$@"      # проверка ввода
+CONFIG_FILE="./colors.conf"
+
+# read conf
+
+if [[ -f "$CONFIG_FILE" ]]; then
+    . "$CONFIG_FILE"
+fi
+
+apply_color_defaults  
+
+# проверка ввода из конфига \ default
+param "$column1_background" "$column1_letter_color" "$column2_background" "$column2_letter_color"  
 info            # собрать данные
-print "$@"      # вывести с цветами
+print "$column1_background" "$column1_letter_color" "$column2_background" "$column2_letter_color"   # вывести с цветами
+
+echo 
+
+print_scheme_line "Column 1 background" "$column1_background" "$is_def_color1_cb"
+print_scheme_line "Column 1 font color" "$column1_letter_color" "$is_def_color1_cl"
+print_scheme_line "Column 2 background" "$column2_background" "$is_def_color2_cb"
+print_scheme_line "Column 2 font color" "$column2_letter_color" "$is_def_color2_cl"
+
+# echo "DBG flags: cb1=$is_def_color1_cb lc1=$is_def_color1_cl cb2=$is_def_color2_cb lc2=$is_def_color2_cl" >&2
